8c1c48412f317e0d277d84665cff77495924975c
jsepu <jsepu@DESKTOP-E7G432T>
2023-02-24 18:11:44 +0200

    Add project files.

---
+++ Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullCRUDApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<Comic> comics = new List<Comic> {
+        new Comic { ComicId = 1, ComicName = "Marvel" },
+        new Comic { ComicId = 2 , ComicName = "DC"}
+        };
+
+        public static List<SuperHero> heros = new List<SuperHero>
+        {
+            new SuperHero
+            {
+                HeroId = 1,
+                FirstName = "Peter",
+                LastName = "Paker",
+                HeroName = "Spiderman",
+                comic = comics[0]
+            },
+            new SuperHero
+            {
+                HeroId = 1,
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                HeroName = "Batman",
+                comic = comics[1]
+            }
+
+        };
+
+    }
+
+}
---
+++ Shared/Comic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullCRUDApp.Shared
+{
+    public class Comic
+    {
+        public int ComicId { get; set; }
+        public string ComicName { get; set; }
+    }
+}
